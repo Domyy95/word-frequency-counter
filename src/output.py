ec75a9fafd2fa8316d452884e7c0f88ed8bfc171
Dominic Crippa <dominic.crippa@gmail.com>
2025-03-05 20:16:41 +0100

    feat: add sums columns when download

--- src/output.py
+++ src/output.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import pandas as pd
 from pandas import DataFrame, ExcelWriter
 
-from model import GroupedWords, col_word, col_frequency
+from model import GroupedWords, col_word, col_frequency, col_sums
 
 xlsx_mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 csv_mime = "text/csv"
@@ -12,7 +12,8 @@ csv_mime = "text/csv"
 
 def _convert_df_to_xlsx(df: DataFrame) -> bytes:
     output = BytesIO()
-    df[col_frequency] = df[col_frequency].applymap(lambda x: str(x).replace(".", ","))
+    df[col_frequency] = df[col_frequency].apply(lambda x: str(x).replace(".", ","))
+    df[col_sums] = df[col_sums].apply(lambda x: str(x).replace(".", ","))
     with ExcelWriter(output, engine="openpyxl") as writer:
         df.to_excel(writer, index=False, sheet_name="Sheet1")
     output.seek(0)
@@ -48,12 +49,14 @@ def get_data_to_download(words_data: list[GroupedWords], file_type: str) -> byte
         return b""
     words_data_joined = join_same_idx_grouped_words(words_data)
     data = []
+    sums = []
     for group in words_data_joined:
         group_data = [(word, freq) for word, freq in group.frequencies.items()]
         df_group = DataFrame(group_data, columns=[col_word, col_frequency])
         df_group[col_frequency] = pd.to_numeric(df_group[col_frequency], errors="coerce")
         # frequency sum row with empty word
         total = df_group[col_frequency].sum()
+        sums.append(total)
         total_row = DataFrame([["", total]], columns=[col_word, col_frequency])
         df_group = pd.concat([df_group, total_row], ignore_index=True)
         df_group[col_frequency] = df_group[col_frequency].apply(
@@ -62,4 +65,8 @@ def get_data_to_download(words_data: list[GroupedWords], file_type: str) -> byte
         data.append(df_group)
 
     final_df = pd.concat(data, ignore_index=True)
+    # add total sum column
+    final_df[col_sums] = ""
+    N = len(sums)
+    final_df.loc[: N - 1, col_sums] = sums
     return convert_df(final_df, file_type)
